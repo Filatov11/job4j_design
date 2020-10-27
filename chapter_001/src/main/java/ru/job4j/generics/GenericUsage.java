504da134a03a7f9df3797cb8a1eb2275180c39b6
Filatov11 <denfilatov@protonmail.com>
2020-10-27 23:58:13 +0300

    4952#345621

--- chapter_001/src/main/java/ru/job4j/generics/GenericUsage.java
+++ chapter_001/src/main/java/ru/job4j/generics/GenericUsage.java
@@ -1,14 +1,45 @@
 package ru.job4j.generics;
-import java.util.ArrayList;
-import java.util.List;
+import ru.job4j.generics.Person;
+
+import java.util.*;
+
 public class GenericUsage {
+
+    public void printRsl(Collection<?> col) {
+        for (Iterator<?> it = col.iterator(); it.hasNext(); ) {
+            Object next = it.next();
+            System.out.println("Текущий элемент: " + next);
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
+        List<Integer> l = List.of(1, 2, 3, 4, 5);
+        new GenericUsage().printRsl(l);
+
+        List list = new ArrayList();
         list.add("first");
         list.add("second");
         list.add("third");
-        for (String s : list) {
-            System.out.println("Current element is: " + s);
+       // list.add(new Person("name", 21, new Date(913716000000L)));
+        System.out.println("Количество элементов в списке: " + list.size());
+        for (int i = 0; i < list.size(); i++) {
+            String s = (String) list.get(i);
+            System.out.println("Текущий элемент: " + s);
         }
+
+
+
     }
+
+
+        public void printInfo(Collection<? extends Person> col) {
+            for (Iterator<? extends Person> it = col.iterator(); it.hasNext(); ) {
+                Person next = it.next();
+                System.out.println(next);
+            }
+        }
+
+
 }
--- chapter_001/src/main/java/ru/job4j/generics/Person.java
+++ chapter_001/src/main/java/ru/job4j/generics/Person.java
@@ -1,5 +1,10 @@
 package ru.job4j.generics;
 import java.util.Date;
 public class Person {
-    
+    private String name;
+    private int age;
+    private Date birthday;
+    public  Person(String name, int age, Date bi ) {
+
+    }
 }
