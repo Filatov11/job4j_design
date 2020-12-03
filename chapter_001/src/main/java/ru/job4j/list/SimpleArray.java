1755e40acb14b13564dfda445e549ff2d8e939c8
Filatov11 <denfilatov@protonmail.com>
2020-12-04 00:47:57 +0300

    158#345598.1

--- chapter_001/src/main/java/ru/job4j/list/SimpleArray.java
+++ chapter_001/src/main/java/ru/job4j/list/SimpleArray.java
@@ -1,57 +1,112 @@
 package ru.job4j.list;
-import java.util.Arrays;
-import java.util.ConcurrentModificationException;
 import java.util.Iterator;
-import java.util.Objects;
+import java.util.*;
 
 
 public class SimpleArray <T> implements Iterable<T> {
     private static final int initSize = 10;
-    private int CurrSize;
+    private int currSize;
     private T[] smpArray;
-    private int CurrNumber;
+    private int currNumber;
+    protected transient int modCount = 0;
+
 
     public SimpleArray() {
-        smpArray =  (T[])new Object[10];
+        smpArray = (T[]) new Object[10];
     }
 
-    public T get(int index){
-        if(Objects.checkIndex(index,smpArray.length) < getSizeOfArray()){
-            return smpArray[index];
-        } else {
-            throw new ArrayIndexOutOfBoundsException();
-        }
+    public T get(int index) {
+
+        Objects.checkIndex(index, currSize);
+
+        return smpArray[index];
+
     }
 
-    public void add(T obj){
-        if(smpArray.length-getSizeOfArray() <= 5){
+    public void add(T obj) {
+
+        if (currSize == smpArray.length) {
             increaseListSize();
         }
-        smpArray[this.CurrSize++] = obj;
+        smpArray[this.currSize++] = obj;
+        modCount++;
     }
+
+
+    public int getSizeOfArray() {
+        return this.currSize;
+    }
+
+
+    private void increaseListSize() {
+        smpArray = Arrays.copyOf(smpArray, smpArray.length * 2);
+        System.out.println("\nNew length: " + smpArray.length);
+    }
+
+    public boolean arrIsEmpty() {
+        boolean empty = true;
+        for (int i = 0; i < smpArray.length; i++) {
+            if (smpArray[i] != null) {
+                empty = false;
+                break;
+            }
+        }
+        return empty;
+    }
+
 
     @Override
     public Iterator<T> iterator() {
-        int modCount = 0;
-        int expectedModCount = modCount;
-
-        if (expectedModCount != modCount) {
-            Iterator<T> iterator = Arrays.stream(smpArray).iterator();
+        if (arrIsEmpty()) throw new NoSuchElementException();
+        Iterator<T> iterator = new SimpleArrayIterator();
+        try {
+            System.out.println("Iterator created");
             return iterator;
-        }   else {
-            throw new ConcurrentModificationException();
+        } catch (NoSuchElementException exception) {
+        }
+        ;
+        return null;
+    }
+
+    private class SimpleArrayIterator implements Iterator {
+        private int position = 0;
+
+        public boolean hasNext() {
+            if (position < smpArray.length)
+                return true;
+            else
+                return false;
         }
 
+        public T next() {
+
+            if (this.hasNext()) {
+                checkForComodification();
+                return smpArray[position++];
+            } else
+
+
+                return null;
+        }
+
+
+
+        final void checkForComodification() {
+          //  if (modCount != expectedModCount)
+         //       throw new ConcurrentModificationException();
+        }
+
+
+
+
+
+        @Override
+        public void remove() {
+
+        }
+
+
     }
 
-   public int getSizeOfArray() {
-        return  this.CurrSize;
-   }
 
-
-
-    private void increaseListSize(){
-        smpArray = Arrays.copyOf(smpArray, smpArray.length*2);
-        System.out.println("\nNew length: "+smpArray.length);
-    }
 }
