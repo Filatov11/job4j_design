2089b11ef69f35042d422a0c5c5bc4ba276c0aa1
Filatov11 <denfilatov@protonmail.com>
2020-12-13 00:10:49 +0300

    159#345599.1

--- chapter_001/src/main/java/ru/job4j/list/AppLinkList.java
+++ chapter_001/src/main/java/ru/job4j/list/AppLinkList.java
@@ -1,13 +1,17 @@
 package ru.job4j.list;
 
 public class AppLinkList {
-    public static void main (String [] args) {
-        CustLinkedList app = new CustLinkedList();
-        app.add(new Person("Fred","Person one"));
-        app.add(new Person("Mark","Person two"));
-        app.add(new Person("Teddy","Person three"));
-        app.add(new Person("Sam","Person four"));
+    public static void main(String[] args) {
+        CustLinkedList<Person> app = new CustLinkedList<Person>();
+        app.add(new Person("Fred", "Person one"));
+        app.add(new Person("Mark", "Person two"));
+        app.add(new Person("Teddy", "Person three"));
+        app.add(new Person("Sam", "Person four"));
 
-        System.out.println(app.get(0));
+
+
+        while (app.iterator().hasNext()) {
+         System.out.println(app.iterator().next().name  );
+       }
     }
 }
--- chapter_001/src/main/java/ru/job4j/list/CustLinkedList.java
+++ chapter_001/src/main/java/ru/job4j/list/CustLinkedList.java
@@ -1,70 +1,133 @@
 package ru.job4j.list;
-import  ru.job4j.list.Node;
 
-import java.lang.management.MemoryNotificationInfo;
-import java.util.Objects;
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 public class CustLinkedList<E> implements Iterable<E> {
-    public static int size;
-    private Node first;
-    private Node last;
+    public static int size = 0;
+    private Node<E> node;
+    private Node<E> currPoint;
 
 
     public CustLinkedList() {
-        first = last = null;
-        size=0;
+        size = 0;
+        currPoint = this.node;
     }
 
 
-    public   void add(E value) {
-        Node new_node = new Node();
-        new_node.setValue(value);
-        if (first == null) {
-            first = new Node();
-            first.setValue(value);
+    public void add(E value) {
+        ConcurrentModification();
+        Node newNode = new Node();
+        newNode.setValue(value);
+        if (newNode == null) {
             return;
         }
-        new_node.setNext(null);
-        Node last = first;
-        while (last.getNext() != null)
-            last = last.getNext();
-        last.setNext(new_node);
+        if (node == null) {
+            node = new Node<E>();
+            node.setValue(value);
+        } else {
+            Node<E> nNode = new Node<E>();
+            nNode.setValue(value);
+            Node<E> lastNode = getLast(node);
+            lastNode.setNext(newNode);
+        }
+        resetIteratorPoint();
         size++;
         return;
 
     }
 
-    public  E get(int index) {
-        int ind = Objects.checkIndex(index,size);
-         Node temp = first;
-         while (temp != null) {
-             if (temp.getKey() == ind) {
-                 return (E)temp;
-             }
-             temp = temp.getNext();
-         }
 
+    private void resetIteratorPoint() {
+        currPoint = this.node;
+    }
+
+    private Node<E> getLast(Node<E> node) {
+
+        Node<E> lastNode = node;
+        if (lastNode.getNext() != null) {
+            return getLast(lastNode.getNext());
+        } else {
+            return lastNode;
+        }
+    }
+
+    public void ConcurrentModification() throws ConcurrentModificationException {
+        if (currPoint != this.node) {
+            throw new ConcurrentModificationException("Concurrent modification");
+        }
+    }
+
+    public E get(int index) {
+        return getNode(index).getValue();
+    }
+
+    public Node<E> getNode(int index) {
+        if (index < 0 || index > this.size - 1) {
+            throw new IndexOutOfBoundsException("Index not available.");
+        }
+        // If index=0 , return head
+        if (index == 0) {
+            return this.node;
+        }
+        // If index= size, return last node
+        if (index == this.size - 1) {
+            return getLast(this.node);
+        }
+        int pointer = 0;
+        Node<E> pointerNode = this.node;
+        while (pointer <= index) {
+            if (pointer == index) {
+                return pointerNode;
+            } else {
+                pointerNode = next(pointerNode);
+                pointer++;
+            }
+        }
         return null;
     }
 
+    private Node<E> next(Node<E> node) {
+        if (node.getNext() != null) {
+            return node.getNext();
+        } else {
+            return null;
+        }
+    }
+
+
     @Override
     public Iterator<E> iterator() {
-        return new Iterator<E>(){
-            private Node current;
+        return new Iterator<E>() {
+            //   private Node current;
 
             @Override
             public boolean hasNext() {
-                return  current != null;
+                {
+                    if (currPoint == null) {
+                        resetIteratorPoint();
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
             }
 
             @Override
             public E next() {
-                Node temp = current;
-                current = current.getNext();
-                return (E) temp;
+                E data = (E) currPoint.getValue();
+                currPoint = currPoint.getNext();
+                return data;
+
             }
         };
     }
 }
 
+
+    /*
+
+
+
+
+*/
