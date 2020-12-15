0376e79b6869c8511e2f95759b8e699ad98124ab
Filatov11 <denfilatov@protonmail.com>
2020-12-15 23:30:23 +0300

    51424#345596

---
+++ chapter_001/src/test/java/ru/job4j/list/DeleteFirstLinkedTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.list;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+
+
+public class DeleteFirstLinkedTest {
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteFirst() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.deleteFirst();
+        linked.iterator().next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenDeleteEmptyLinked() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.deleteFirst();
+    }
+
+    @Test
+    public void whenMultiDelete() {
+        ForwardLinked<Integer> linked = new ForwardLinked<>();
+        linked.add(1);
+        linked.add(2);
+        assertThat(linked.deleteFirst(), is(1));
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+    }
+}
+
