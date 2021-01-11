d626670d99bcd7816b903eb7955c3f64a57f6e23
Filatov11 <denfilatov@protonmail.com>
2021-01-11 23:29:05 +0300

    999#345626

---
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,19 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class User {
+    String name;
+    int children;
+    Calendar birthday;
+
+    public User (String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+
+
+}
