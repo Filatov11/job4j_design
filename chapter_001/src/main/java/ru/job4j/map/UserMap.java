3fca00701a915bf715bbe5e593c4e85ce8e2599b
Filatov11 <denfilatov@protonmail.com>
2021-01-12 23:21:49 +0300

    1005#345630

--- chapter_001/src/main/java/ru/job4j/map/UserMap.java
+++ chapter_001/src/main/java/ru/job4j/map/UserMap.java
@@ -1,5 +1,25 @@
 package ru.job4j.map;
 
+import java.util.*;
+
 public class UserMap {
+    Object obj;
+    User user;
+    public  static void main (String[] args) {
+        Map<User,Object>  usrMap = new HashMap<User, Object>();
+        usrMap.put(new User("Nik", 2, new GregorianCalendar(1976, 10 , 02)), new Object());
+        usrMap.put(new User("Tom", 3, new GregorianCalendar(1976, 05 , 10)), new Object());
+
+        for (Map.Entry<User,Object>  item :    usrMap.entrySet())
+
+        System.out.println( item.getKey().birthday.toString() + " " +  item.getKey().children  + " "
+                 + item.getKey().name + " val " + item.getValue().toString() );
+
+    }
+
+
+
+
+
 
 }
