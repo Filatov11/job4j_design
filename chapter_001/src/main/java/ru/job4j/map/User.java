2e8b68c9b500f5a8c2113768849c4ca5871b23f8
Filatov11 <denfilatov@protonmail.com>
2021-01-13 21:40:58 +0300

    999#345626.1

--- chapter_001/src/main/java/ru/job4j/map/User.java
+++ chapter_001/src/main/java/ru/job4j/map/User.java
@@ -4,10 +4,33 @@ import java.util.Calendar;
 import java.util.HashMap;
 
 public class User {
-    String name;
-    int children;
-    Calendar birthday;
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+    private String name;
+    private  int children;
+    private  Calendar birthday;
     public User (String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
