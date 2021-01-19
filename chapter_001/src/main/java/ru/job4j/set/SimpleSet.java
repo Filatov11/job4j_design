f2efc2dec4657854eba9417b1148499b92d44a6a
Filatov11 <denfilatov@protonmail.com>
2021-01-19 23:03:46 +0300

    996#345623.2

--- chapter_001/src/main/java/ru/job4j/set/SimpleSet.java
+++ chapter_001/src/main/java/ru/job4j/set/SimpleSet.java
@@ -1,30 +1,35 @@
 package ru.job4j.set;
-import  ru.job4j.list.SimpleArray;
+
+import ru.job4j.list.SimpleArray;
+
 import java.util.Iterator;
 import java.util.Objects;
 
 public class SimpleSet<E> implements Iterable<E> {
 
- private  SimpleArray <E> arrForSet = new SimpleArray<E>();
+    private SimpleArray<E> arrForSet = new SimpleArray<E>();
+
     public boolean checkElement(E elem) {
-       boolean res = false;
-        for ( E part :
-             arrForSet) {
-           if ((part.equals(elem)) || (Objects.equals (elem, null)))  { res = true; break;}
-         //  else  {res = true; break; }
+        boolean res = false;
+        for (E part :
+                arrForSet) {
+            if (Objects.equals(elem, part)) {
+                res = true;
+                break;
+            }
         }
         return res;
     }
 
     void add(E e) {
-    if (!checkElement(e)) {
-        arrForSet.add(e);
-    }
+        if (!checkElement(e)) {
+            arrForSet.add(e);
+        }
 
     }
 
     @Override
     public Iterator<E> iterator() {
-     return  arrForSet.iterator();
+        return arrForSet.iterator();
     }
 }
