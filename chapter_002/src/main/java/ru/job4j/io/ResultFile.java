3ee5132a32825a8c7f2ee0ce7d6b1bea48709fdd
Filatov11 <denfilatov@protonmail.com>
2021-02-27 01:07:49 +0300

    252488#345646.1

--- chapter_002/src/main/java/ru/job4j/io/ResultFile.java
+++ chapter_002/src/main/java/ru/job4j/io/ResultFile.java
@@ -1,4 +1,5 @@
 package ru.job4j.io;
+import java.io.File;
 import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
@@ -9,7 +10,38 @@ public class ResultFile {
       e.printStackTrace();
   }
 
+        try
+        {  File  file = new File("result.txt");
+            FileOutputStream  fos = new FileOutputStream( file, true);
+           fos = new FileOutputStream(file, true);
+            fos.write(("\n ====================  //////////////  ============= ").getBytes());
+            fos.write(("\n ").getBytes());
+            int x = 9;
+            fos.write("   ".getBytes());
+            for(int k = 0; k<=x ;k++ ) {
+                fos.write(( String.format("%3d", k) + "  ").getBytes());
+            }
+
+            fos.write(("\n +------------------------------------------------- \n ").getBytes());
+            for(int i = 0 ;i<=x ;i++) {
+                fos.write((i+ "| ").getBytes());
+                for(int j=0;j<=x ;j++) {
+                    fos.write((String.format("%3d", j*i) + "  ").getBytes());
+                }
+               fos.write(("\n " ).getBytes());
+            }
+            fos.write(("\n").getBytes());
+            fos.write(("\n\n").getBytes());
 
 
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+        //String.format("%3d", k)
     }
 }
