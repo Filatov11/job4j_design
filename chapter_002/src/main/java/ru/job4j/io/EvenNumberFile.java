ff3c7a92b126f47742c198dc58f73482b649ff92
Filatov11 <denfilatov@protonmail.com>
2021-02-27 18:41:20 +0300

    4898#345656

---
+++ chapter_002/src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream in = new FileInputStream("event.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                checkValue(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void checkValue(String value) {
+        int val = Integer.parseInt(value);
+        if (val % 2 == 0) {
+            System.out.println("value " + val + " is parity");
+        } else {
+            System.out.println("value " + val + " is not parity");
+        }
+    }
+}
