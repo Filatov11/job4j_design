54e50b90af967893e482e751eef47ecc041dfb2b
Filatov11 <denfilatov@protonmail.com>
2021-03-01 00:49:11 +0300

    252490#345648

---
+++ chapter_002/src/main/java/ru/job4j/io/LogFilterSave.java
@@ -0,0 +1,44 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogFilterSave {
+    public static List<String> filter(String file) {
+        String line;
+        List<String> log = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            while ((line = in.readLine()) != null) {
+                if (checkResponse(line)) {
+                    log.add(line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return log;
+    }
+
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            for (String item : log) {
+                out.write(item);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> log = filter("log.txt");
+        save(log, "404.txt");
+    }
+
+    public static boolean checkResponse(String resp) {
+        return resp.contains("404");
+    }
+}
