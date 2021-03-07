bb5d94f785e7efb1d5d0938476ba8bcce5f44bf5
Filatov11 <denfilatov@protonmail.com>
2021-03-07 10:00:56 +0300

    106929#345643

---
+++ chapter_002/src/main/java/ru/job4j/io/SearchJS.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+public class SearchJS {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get("/home/aster/IdeaProjects");
+        search(start, "js").forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, String ext) throws IOException {
+        SearchFiles searcher = new SearchFiles(p -> p.toFile().getName().endsWith(ext));
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
