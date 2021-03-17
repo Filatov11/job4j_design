b9614a74d8c841750ee4e3bf3dc6d7c1c91078c6
Filatov11 <denfilatov@protonmail.com>
2021-03-17 20:19:16 +0300

    295518#345650.1

--- chapter_002/src/main/java/ru/job4j/io/ArgsName.java
+++ chapter_002/src/main/java/ru/job4j/io/ArgsName.java
@@ -1,38 +1,37 @@
 package ru.job4j.io;
+
 import java.util.HashMap;
 import java.util.Map;
+
 public class ArgsName {
     private final Map<String, String> values = new HashMap<>();
 
     public String get(String key) {
-        return values.containsKey(key) ? values.get(key): null;
+        return values.get(key);
     }
 
     private void parse(String[] args) {
         String key;
         String value;
-        if ((args.length == 0 ) || (args == null)) { values.put(null, null);}
-     for (String ar : args) {
-         if ((ar.contains("="))) {
-             int posA = ar.indexOf('=');
-             key = ar.substring(1, posA);
-             value = ar.substring(posA + 1);
-             values.put(key, value);
-         }
-     }
+        String[] mapArg;
+        for (String ar : args) {
+            if ((ar.contains("="))) {
+                mapArg = ar.split("=");
+                values.put(mapArg[0].substring(1), mapArg[1]);
+            }
+        }
     }
 
     public static ArgsName of(String[] args) {
-ArgsName names = new ArgsName();
-names.parse(args);
-return names;
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
     }
 
     public static void main(String args[]) {
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
         System.out.println(jvm.get("Xmx"));
-
-        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
         System.out.println(zip.get("out"));
     }
 }
