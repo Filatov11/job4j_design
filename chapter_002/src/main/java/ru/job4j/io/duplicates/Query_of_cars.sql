37e159d7edbb55de6441e3845ed58a183bc997c3
Filatov11 <denfilatov@protonmail.com>
2021-05-18 00:28:33 +0300

    1733#345584.1

--- chapter_002/src/main/java/ru/job4j/io/duplicates/Query_of_cars.sql
+++ chapter_002/src/main/java/ru/job4j/io/duplicates/Query_of_cars.sql
@@ -1,6 +1,8 @@
 ﻿1)
 
-select nmr.model_name as "Car model", eg.engine_type as "Type of engine", tm.type as "Type of transmission", vb.type as "Type of car body" from nissan_models_range nmr,  engines eg , transmissions tm, vehicle_body vb
+select nmr.model_name as "Car model", eg.engine_type as "Type of engine", 
+           tm.type as "Type of transmission", vb.type as "Type of car body" 
+from nissan_models_range nmr,  engines eg , transmissions tm, vehicle_body vb
 where nmr.gearbox_id = tm.tm_id
 and nmr.vehicle_body_id = vb.vb_id
 and nmr.engine_id = eg.engine_id
@@ -10,14 +12,36 @@ and nmr.engine_id = eg.engine_id
 
 Parts not used in car_model
 
-select nmr.model_name as "Car model"  , eg.engine_type as "Type of engine"
-from nissan_models_range nmr  left join engines eg  on  nmr.engine_id = eg.engine_id
-and nmr.model_name <> 'Nissan Infiniti'
 
-select nmr.model_name as "Car model"  , tm.type as "Type of transmission"
-from nissan_models_range nmr  left join transmissions tm  on  nmr.gearbox_id = tm.tm_id
-and nmr.model_name <> 'Nissan Infiniti'
 
-select nmr.model_name as "Car model"  , vb.type as "Type of car body"
-from nissan_models_range nmr  left join vehicle_body vb  on nmr.vehicle_body_id = vb.vb_id
-and nmr.model_name <> 'Nissan Infiniti'
+
+ 
+select * from  engines eg where engine_id not in ( select nmr.engine_id-- eg.engine_type as "Type of engine"
+from nissan_models_range nmr 
+ where nmr.model_name = 'Nissan Infiniti'
+and nmr.engine_id is not null
+)
+
+
+
+
+
+
+
+
+
+
+
+select * from  transmissions tm where tm.tm_id not in ( select nmr.gearbox_id 
+from nissan_models_range nmr 
+ where nmr.model_name = 'Nissan Infiniti'
+and nmr.gearbox_id is not null
+)
+
+
+select * from  vehicle_body vb where vb.vb_id not in ( select nmr.vehicle_body_id 
+from nissan_models_range nmr 
+ where nmr.model_name = 'Nissan Infiniti'
+and nmr.gearbox_id is not null
+)
+
